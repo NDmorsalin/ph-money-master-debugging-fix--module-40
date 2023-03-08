4cd9c058d3496a14939d1f1efc2f20806ad166ff
ND Morsalin <morsalin056@gmail.com>
2023-03-08 05:03:46 -0800

    money-master-debugging-fix

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const calculateExpense = () => {
-  const income = document.querySelector("income").value;
-  const food = document.querySelector("food").value;
-  const rent = document.querySelector("rent").value;
-  const clothes = document.querySelectorAll("clothes").value;
+  const income = document.querySelector("#income").value;
+  const food = document.querySelector("#food").value;
+  const rent = document.querySelector("#rent").value;
+  const clothes = document.querySelector("#clothes").value;
 
   if (
     income < 0 ||
@@ -20,9 +20,8 @@ const calculateExpense = () => {
   // calculate expense
   const expense =
     parseInt(food) + parseInt(rent) + parseInt(clothes);
-
   // calculate balance
-  const balance = parseInt(income.value) - expense;
+  const balance = parseInt(income) - expense;
   //   validate income
   if (expense > income.value) {
     alert("Expenses cannot be more than income");
@@ -34,14 +33,16 @@ const calculateExpense = () => {
 };
 
 const calculateSavings = () => {
+  const income = document.querySelector("#income").value;
+
   // calculate saving amount
   const savePercentage = document.getElementById("save").value;
 //   Validate saving percentage value
   if (savePercentage < 0) {
     alert("Provide positive saving value");
   }
-  const savingAmount = (savePercentage / 100) + income;
-
+  const savingAmount = (savePercentage / 100) * income;
+  
   // calculate remaining balance
   const balance = document.getElementById("balance").innerText;
   const remainingBalance = balance - savingAmount;
@@ -52,6 +53,6 @@ const calculateSavings = () => {
   } else {
     // view saving amount and remaining balance
     document.getElementById("saving-amount").innerText = savingAmount;
-    document.getElementById("remaining-balance").innerText = remainingBalance;
+    document.querySelector(".remaining-balance").innerText = remainingBalance;
   }
 };
